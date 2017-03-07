d09c105f1aa6e3a87c0c339fb0b29e4d86b8e286
Kelley Danger <kelleyscanlon@gmail.com>
2017-03-06 21:00:19 -0800

    working on dynamic image loading

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,62 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import classNames from 'classnames';
-import HandImg from '../public/assets/images/hand.png';
+
 import '../public/assets/css/custom.scss';
 
+import HandImg from './assets/hand.png';
+
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			decalPack: ''
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		const decalSet = event.target.value;
+    this.setState({decalPack: decalSet});
+    console.log('decal pack changed');
+    //console.log('decal pack:' + decalSet);
+    ReactDOM.render(
+		  <Decals decalPack={decalSet} renderDecals={this.renderDecals} />,
+		  document.getElementById('decals')
+		);
+  }
+
+  renderDecals(decalPack) {
+  	console.log(decalPack);
+  	console.log("LOADING IMAGES");
+  	
+  	// pathEmoticons = require.context(`../public/assets/images/decals/Emoticons`, true, /^\.\/.*\.png$/);
+  	//var pathDecals = require.context(`./client/public/assets/images/decals/${decalPack}`, true, /^\.\/.*\.png$/);
+  	//var requireDecals = require.context("../public/assets/images/decals", true, /^\.\/.*\.png$/);
+  	//var Emoticons = requireDecals("./Emoticons/");
+  	//console.log(Emoticons);
+  	//var ds = pathEmoticons.keys().map(pathEmoticons);
+  	//var decals = requireDecals.keys().map(requireDecals);
+  	//console.log(requireDecals);
+  	//if (decalPack.size < 1) {
+  	//	return false;
+  	//}
+
+  	//return decals.map((decal, index) => (
+  	//	<img src={decal} id={index} />
+  	//));
+  }
+
   render() {
+  	var decalPack = "Halloween"
+  	var pathDecals = require.context(`./client/public/assets/images/decals/${decalPack}`, true, /^\.\/.*\.png$/);
     return (
      <div>
         <Introduction />
         <Hand />
+        <DecalList onChange={this.handleChange} renderDecals={this.renderDecals}/>
+        <div id="decals"></div>
       </div>);
   }
 }
@@ -41,7 +89,6 @@ class Hand extends React.Component {
 
 class Fingernail extends React.Component {
 	render() {
-		console.log(this.props.finger);
 		let classes = classNames(
 			'fingernail', this.props.finger
 		);
@@ -53,7 +100,33 @@ class Fingernail extends React.Component {
 }
 
 class DecalList extends React.Component {
+	render() {
+		return (
+			<select onChange={this.props.onChange}>
+			  <option value="Animals" defaultValue>Animals</option>
+			  <option value="Emoticons">Emoticons</option>
+			  <option value="Halloween">Halloween</option>
+			  <option value="SacredGeometry">Sacred Geometry</option>
+			</select>
+		);
+	}
 
+}
+
+
+class Decals extends React.Component {
+	render() {
+		console.log('inside decals');
+		var decalPack = this.props.decalPack;
+		console.log(decalPack);
+		const decals = this.props.renderDecals(decalPack);
+
+		return (
+			<div id="decals">
+				{decals}
+			</div>
+		)
+	}
 }
 
 class Decal extends React.Component {
--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,21 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    path: __dirname +'/build',
+    publicPath: "assets/",
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader' },
+      { 
+        test: /\.(jpe?g|png|gif|svg)$/i, 
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[name].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ] 
+      },
       {
         test: /\.scss$/,
         use: [{
