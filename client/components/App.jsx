c5852860107ebb5cb37cde77cc3a0c6127f85e35
Kelley Danger <kelleyscanlon@gmail.com>
2017-03-11 16:32:56 -0800

    Added Hand Color Change

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import classNames from 'classnames';
+import { CirclePicker } from 'react-color';
 
 import '../public/assets/css/custom.scss';
 
-import HandImg from 'images/hand.png';
+//import HandImgs from "images/hands/hands.js";
 import DecalImgs from 'images/decals/decals.js'
 
 import merge from 'lodash/merge';
@@ -14,6 +15,10 @@ class App extends React.Component {
 		super();
 		this.state = {
 			decalPack: 'Animals',
+			handSettings: {
+				handColor: '#F8D0B7',
+				fingernailColor: '#FCECE0'
+			},
 			fingernailDecals: {
 				thumb: 'https://img.clipartfest.com/0d6a3240800caf71073288e1df38bf8a_cute-panda-png-c-cute-pnda-clipart-png_128-128.png',
 				pointer: '',
@@ -27,6 +32,7 @@ class App extends React.Component {
 		this.handleChange = this.handleChange.bind(this);
 		this.setCurrentFinger = this.setCurrentFinger.bind(this);
 		this.setCurrentFingernailDecal = this.setCurrentFingernailDecal.bind(this);
+		this.setHandColor = this.setHandColor.bind(this);
 
 	}
 
@@ -42,6 +48,15 @@ class App extends React.Component {
 		})
 	}
 
+	setHandColor(color) {
+		const newHandColor = Object.assign({}, this.state.handSettings, {handColor: color.hex})
+		this.setState({
+			handSettings: newHandColor
+		});
+		console.log("HAND COLOR: " + this.state.handSettings.handColor);
+		console.log("fingernaiL COLOR: " + this.state.handSettings.fingernailColor);
+	}
+
 	handleChange(event) {
 		const decalPack = event.target.value;
     this.setState({decalPack: decalPack});
@@ -51,7 +66,8 @@ class App extends React.Component {
     return (
      <div>
         <Introduction />
-        <Hand setCurrentFinger={this.setCurrentFinger} fingernailDecals = {this.state.fingernailDecals}/>
+        <HandSettings handColor={this.state.handSettings.handColor} setHandColor={this.setHandColor}/>
+        <Hand setCurrentFinger={this.setCurrentFinger} fingernailDecals={this.state.fingernailDecals} handSettings={this.state.handSettings}/>
         <DecalSelectList onChange={this.handleChange} />
         <DecalList setCurrentFingernailDecal={this.setCurrentFingernailDecal} decalPack={this.state.decalPack} />
       </div>);
@@ -71,7 +87,10 @@ class Introduction extends React.Component {
 
 class Hand extends React.Component {
 	render() {
-		const fingernailDecals = this.props.fingernailDecals
+		const fingernailDecals = this.props.fingernailDecals;
+		// remove '#' from hex for hand image names
+		const handColor = this.props.handSettings.handColor.replace("#", "").toLowerCase();
+
 		return (
 			<div id="handWrapper">
 				<Fingernail setCurrentFinger={this.props.setCurrentFinger} decal={fingernailDecals.thumb} finger="thumb" />
@@ -79,7 +98,7 @@ class Hand extends React.Component {
 				<Fingernail setCurrentFinger={this.props.setCurrentFinger} decal={fingernailDecals.middle} finger="middle" />
 				<Fingernail setCurrentFinger={this.props.setCurrentFinger} decal={fingernailDecals.ring} finger="ring" />
 				<Fingernail setCurrentFinger={this.props.setCurrentFinger} decal={fingernailDecals.pinky} finger="pinky" />
-				<img id="handImg" src={HandImg} />
+				<img id="handImg" src={`./client/public/assets/images/hands/hand_${handColor}.png`} />
 			</div>
 		);
 	}
@@ -171,4 +190,22 @@ class Decal extends React.Component {
 		)
 	}
 }
+
+class HandSettings extends React.Component {
+	handleChange (color, e) {
+		this.props.setHandColor(color);
+	}
+
+	render() {
+		const handColors = ["#6b4b3c", "#925946", "#bd8e7a", "#c6a07b", "#d19475", "#e9d1b5", "#f6bd9f", "#f8d0b7", "#f9ddd2"]
+		return(
+			<div id="handSettings">
+				<h4>Hand Color</h4>
+				<CirclePicker color={this.props.handColor} colors={handColors} onChange={this.handleChange.bind(this)} />;
+				<h4>Nail Color</h4>
+			</div>
+			)
+		}
+}
+
 export default App
